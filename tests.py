3b48597e81a3df157c74fbff417b730d4e9f96c0
seok <wat0209@naver.com>
2023-01-02 20:22:35 +0900

    ui 추가해보기

--- tests.py
+++ tests.py
@@ -1,1 +1,23 @@
-print("안녕")
+import sys
+from PyQt5.QtWidgets import *
+import pyqtgraph as pg
+import numpy as np
+from PyQt5 import uic
+
+form_class = uic.loadUiType("graphui.ui")[0]
+
+
+class GraphWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        w = pg.PlotWidget(background='w')
+        self.setCentralWidget(w)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    graph = GraphWidget()
+    graph.show()
+    app.exec_()
