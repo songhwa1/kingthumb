c481458b12a8f91fd76acb3c8aaf0272393d5e1e
songhwa1 <121270461+songhwa1@users.noreply.github.com>
2023-01-05 15:58:19 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -15,17 +15,11 @@ class Search(QWidget, form_widget):
         self.setupUi(self)
         # 시그널과 함수 연결
         self.police_pushButton.clicked.connect(self.searchPolice)
+        self.police_lineEdit.returnPressed.connect(self.searchPolice)
         self.crime_pushButton.clicked.connect(self.searchCrime)
+        self.crime_tableWidget.doubleClicked.connect(self.cellclicked)
+        self.crime_lineEdit.returnPressed.connect(self.searchCrime)
 
-        # MySQL에서 import 해오기
-        conn = pymysql.connect(host='127.0.0.1',
-                               port=3306,
-                               user='root',
-                               password='0000',
-                               db='safety')
-        a = conn.cursor()
-        a.execute("SELECT * from `safety`.`경찰서1`")
-        police = a.fetchall()
 
         # MySQL로 가공한 파일 csv파일로 만들기
         # f= open("경찰서1.csv",'w',encoding = "utf-8",newline='')
@@ -33,12 +27,6 @@ class Search(QWidget, form_widget):
         # for i in police:
         #     wr.writerow(i)
         # f.close()
-
-        # import해온 데이터 리스트화
-        police_list1 = list(police)
-        self.police_info = []
-        for i in police_list1:
-            self.police_info.append(list(i))
 
         # tableWidget 열 넓이 조절
         self.police_tableWidget.setColumnWidth(0, 121)
@@ -61,15 +49,20 @@ class Search(QWidget, form_widget):
 
     def searchPolice(self):       # 경찰서 검색하는 함수
         word = self.police_lineEdit.text()      # 검색어
-        search_list = []    # 빈 리스트 생성
-        for i in range(len(self.police_info)):
-            if word in self.police_info[i][1]:
-                search_list.append(self.police_info[i])     # 빈 리스트에 검색어가 포함된 항목 추가
+        # MySQL에서 import 해오기
+        conn = pymysql.connect(host='127.0.0.1',
+                               port=3306,
+                               user='root',
+                               password='0000',
+                               db='safety')
+        a = conn.cursor()
+        a.execute(f"SELECT * FROM safety.경찰서1 where 지방청 like '%{word}%'")
+        police = a.fetchall()
 
         # 테이블위젯에 보여주기
         Row = 0
-        self.police_tableWidget.setRowCount(len(search_list))
-        for s in search_list:
+        self.police_tableWidget.setRowCount(len(police))
+        for s in police:
             self.police_tableWidget.setItem(Row, 0, QTableWidgetItem(s[1]))
             self.police_tableWidget.setItem(Row, 1, QTableWidgetItem(s[2]))
             self.police_tableWidget.setItem(Row, 2, QTableWidgetItem(s[3]))
@@ -78,30 +71,42 @@ class Search(QWidget, form_widget):
             self.police_tableWidget.setItem(Row, 5, QTableWidgetItem(s[6]))
 
             Row += 1
+        conn.close()
 
     def searchCrime(self):      # 범죄 검색하는 함수
         word = self.crime_lineEdit.text()       # 검색어
-        crime_info =[]      # 빈 리스트 생성
-        with open('범죄.csv', 'r', encoding='cp949', newline='') as f:
-            rdr = csv.reader(f)
-            for line in rdr:
-                crime_info.append(line)
-        search_crime = []       # 빈 리스트 생성, 검색어가 포함된 항목 추가
-        for i in range(len(crime_info)):
-            if word in crime_info[i][1]:
-                search_crime.append(crime_info[i])
+
+        # MySQL에서 import 해오기
+        conn = pymysql.connect(host='127.0.0.1',
+                               port=3306,
+                               user='root',
+                               password='0000',
+                               db='safety')
+        a = conn.cursor()
+        a.execute(f"SELECT * FROM safety.범죄건수 where 경찰서 like'%{word}%'")
+        crime = a.fetchall()
 
         Row = 0
-        self.crime_tableWidget.setRowCount(len(search_crime))
-        for s in search_crime:
-            self.crime_tableWidget.setItem(Row, 0, QTableWidgetItem(s[0]))
+        self.crime_tableWidget.setRowCount(len(crime))
+        for s in crime:
+            self.crime_tableWidget.setItem(Row, 0, QTableWidgetItem(str(s[0])))
             self.crime_tableWidget.setItem(Row, 1, QTableWidgetItem(s[1]))
-            self.crime_tableWidget.setItem(Row, 2, QTableWidgetItem(s[2]))
-            self.crime_tableWidget.setItem(Row, 3, QTableWidgetItem(s[3]))
-            self.crime_tableWidget.setItem(Row, 4, QTableWidgetItem(s[4]))
-            self.crime_tableWidget.setItem(Row, 5, QTableWidgetItem(s[5]))
+            self.crime_tableWidget.setItem(Row, 2, QTableWidgetItem(str(s[2])))
+            self.crime_tableWidget.setItem(Row, 3, QTableWidgetItem(str(s[3])))
+            self.crime_tableWidget.setItem(Row, 4, QTableWidgetItem(str(s[4])))
+            self.crime_tableWidget.setItem(Row, 5, QTableWidgetItem(str(s[5])))
 
             Row += 1
+
+    def cellclicked(self, event):
+        reply = QMessageBox.question(self, "알림", "추가, 삭제 탭으로 이동 하시겠습니까?",
+        QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+        if reply == QMessageBox.Yes:
+            self.tabWidget.setCurrentIndex(2)
+        else:
+            pass
+
+    # def addlabel(self):
 
 
 if __name__ == "__main__":
